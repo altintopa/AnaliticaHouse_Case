cc9bf24931c0665e0e1c9043384991e74004c35c
Ahmet A <83283552+altintopa@users.noreply.github.com>
2023-03-22 13:20:11 +0300

    Initial Commit

---
+++ AnaliticaHouse_Case/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnaliticaHouse_Case
+//
+//  Created by Ahmet ALTINTOP on 22.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
